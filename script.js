d9aa363f8fed7ed9c6f0da66960b944c3d90e159
Yuvaraj <64359176+yuvaraj-1991@users.noreply.github.com>
2020-05-26 15:38:29 +0530

    Seat selection and count with pricing update done.

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-// Select Movie
-const movieValue = document.getElementById('movie')
+//UI Variables 
 
-//Select seats and change color
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
 
-const seat = document.querySelectorAll('.container')
+let ticketPrice = +movieSelect.value
 
-console.log(seat)
+//Update total and count
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const selectedSeatsCount = selectedSeats.length
+    
+    count.innerText = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+}
+
+//Movie select Event Listener
+movieSelect.addEventListener('change',function(e){
+    ticketPrice = +e.target.value
+    updateSelectedCount()
+})
+
+
+//Event Listener Seat click
+
+container.addEventListener('click',function(e){
+
+    //Selecting seats using conditions
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+        updateSelectedCount()
+    }
+    
+})
